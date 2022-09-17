cdfdf693d5b10566d400c33cb5e8c63a67d2f7b3
AMAN KUMAR SAINI <110121353+amansaini005@users.noreply.github.com>
2022-09-17 22:41:59 +0530

    first

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 from tensorflow.keras.models import load_model
 import numpy as np
-import base 64
+
 
 tokenizer = pickle.load(open('token.pkl', 'rb'))
 model = load_model('words.h5')
